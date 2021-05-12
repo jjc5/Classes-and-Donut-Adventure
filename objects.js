eec872be55bf117fd7cde752f28e240c599642d0
Jason C <jayingmantis.555@gmail.com>
2021-05-11 21:18:35 -0500

    created hamster class

--- objects.js
+++ objects.js
@@ -1,2 +1,16 @@
-dc
-awd
+class Hamster {
+  constructor(name){ // name is the parameter
+    this.owner = '';
+    this.name = name;
+    this.price = 15;
+  }
+  wheelRun(){
+    console.log('squeak squeak');
+  }
+  eatFood(){
+    console.log('nibble nibble');
+  }
+  getPrice(){
+    return this.price;
+  }
+}
