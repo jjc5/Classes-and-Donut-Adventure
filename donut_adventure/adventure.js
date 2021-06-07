620c065187bffeb709065d4777ed932895db1659
Jason C <jayingmantis.555@gmail.com>
2021-06-07 16:24:20 -0500

    Created our hero Dougie

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -1,1 +1,23 @@
-console.log('hello')
+class Hero {
+  constructor(name, health, weapons, catchPhrases){
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      sprinkleSpray: 5,
+      sugarShock: 10,
+    }
+    this.catchPhrases = ['i\'m fresher than day old pizza',
+ 'you can\'t count my calories'];
+  }
+  talkSass(){
+    console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]);
+  }
+  announceHealth(){
+    console.log(this.health)
+  }
+  fight(){
+    console.log('i\'m ready to rumble')
+  }
+}
+
+const dougie = new Hero('Dougie the Donut');
